265eafe122f44e368f2193e1d45996d378010ced
u01dkr <u01dkr@gmail.com>
2026-04-05 14:16:17 +0100

    Google Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,26 @@
 import '../styles/globals.css';
+import Script from 'next/script';
+
+const GA_ID = 'G-24SR7P2GXT';
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${GA_ID}', {
+            page_path: window.location.pathname,
+          });
+        `}
+      </Script>
+      <Component {...pageProps} />
+    </>
+  );
 }
