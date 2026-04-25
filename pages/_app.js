c460f3ff01b44583c05a295f84babdb414a06835
u01dkr <u01dkr@gmail.com>
2026-04-25 17:39:01 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,18 @@
 import '../styles/globals.css';
 import Script from 'next/script';
+import Head from 'next/head';
 
 const GA_ID = 'G-24SR7P2GXT';
 
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1, viewport-fit=cover"
+        />
+      </Head>
       <Script
         src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
         strategy="afterInteractive"
